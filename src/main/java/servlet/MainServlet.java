3b28c35636288298548660a685a13931aaa81cac
MarinaBrovkina <mbrovk@gmail.com>
2024-05-05 14:14:25 +0300

    Third commit

--- src/main/java/servlet/MainServlet.java
+++ src/main/java/servlet/MainServlet.java
@@ -19,25 +19,6 @@ public class MainServlet extends HttpServlet {
     private final String POST_METHOD = "POST";
     private final String DELETE_METHOD = "DELETE";
 
-    @Configuration
-    public class Config {
-
-        @Bean
-        public PostRepository postRepository() {
-            return new PostRepository();
-        }
-
-        @Bean
-        public PostService postService(PostRepository repository) {
-            return new PostService(repository);
-        }
-
-        @Bean
-        public PostController postController(PostService service) {
-            return new PostController(service);
-        }
-    }
-
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) {
 
