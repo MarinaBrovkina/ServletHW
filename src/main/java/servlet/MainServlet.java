5de5fd46494914c86ab425078886366f059a443b
MarinaBrovkina <mbrovk@gmail.com>
2024-05-12 20:46:31 +0300

    commit

--- src/main/java/servlet/MainServlet.java
+++ src/main/java/servlet/MainServlet.java
@@ -1,13 +1,10 @@
 package servlet;
 
 import controller.PostController;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import repository.PostRepository;
 import service.PostService;
-import service.Service;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,13 +12,17 @@ import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
     private PostController controller;
+    private final String POSTS_PATH = "/api/posts";
+    private final String GET_METHOD = "GET";
+    private final String POST_METHOD = "POST";
+    private final String DELETE_METHOD = "DELETE";
 
-    @Override
-    public void init() {
-        ApplicationContext context = new AnnotationConfigApplicationContext("java");
-        controller = context.getBean(PostController.class);
-    }
-
+    //    @Override
+//    public void init() {
+//        final var repository = new PostRepository();
+//        final var service = new PostService(repository);
+//        controller = new PostController(service);
+//    }
     @Configuration
     public class Config {
 
@@ -43,9 +44,45 @@ public class MainServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) {
-        Service service = new Service(controller);
-        service.service(req, resp);
+
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            switch (method) {
+                case GET_METHOD:
+                    if (path.equals(POSTS_PATH)) {
+                        controller.all(resp);
+                    } else if (path.matches(POSTS_PATH + "/\\d+")) {
+                        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                        controller.getById(id, resp);
+                    } else {
+                        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                    }
+                    break;
+                case POST_METHOD:
+                    if (path.equals(POSTS_PATH)) {
+                        controller.save(req.getReader(), resp);
+                    } else {
+                        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                    }
+                    break;
+
+                case DELETE_METHOD:
+                    if (path.matches(POSTS_PATH + "/\\d+")) {
+                        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                        controller.removeById(id, resp);
+                    } else {
+                        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                    }
+                    break;
+                default:
+                    resp.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
     }
 }
-
 
