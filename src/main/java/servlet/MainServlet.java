d4f153a101353bb449ceb9a88ffb30693ddda270
MarinaBrovkina <mbrovk@gmail.com>
2024-05-12 21:29:51 +0300

    commit

--- src/main/java/servlet/MainServlet.java
+++ src/main/java/servlet/MainServlet.java
@@ -3,13 +3,15 @@ package servlet;
 import controller.PostController;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import repository.PostRepository;
+import service.PostService;
 import service.Service;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 
 public class MainServlet extends HttpServlet {
@@ -20,6 +22,26 @@ public class MainServlet extends HttpServlet {
         ApplicationContext context = new AnnotationConfigApplicationContext("java");
         controller = context.getBean(PostController.class);
     }
+
+    @Configuration
+    public class Config {
+
+        @Bean
+        public PostController postController() {
+            return new PostController(postService());
+        }
+
+        @Bean
+        public PostService postService() {
+            return new PostService(postRepository());
+        }
+
+        @Bean
+        public PostRepository postRepository() {
+            return new PostRepository();
+        }
+    }
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) {
         Service service = new Service(controller);
